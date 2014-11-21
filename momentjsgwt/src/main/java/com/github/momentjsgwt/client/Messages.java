9b734c4ebdf0e72a86d476314c70ffb5f8e414ea
Patrice de Saint Steban <patou.de.saint.steban@gmail.com>
2014-11-21 15:27:00 +0100

    Create project

---
+++ momentjsgwt/src/main/java/com/github/momentjsgwt/client/Messages.java
@@ -0,0 +1,10 @@
+package com.github.momentjsgwt.client;
+
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  @DefaultMessage("Entrer votre nom")
+  String nameField();
+
+  @DefaultMessage("Envoyer")
+  String sendButton();
+}
